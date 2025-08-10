47dc1a1c98078a7fc787e47fe956dd48182b64d3
pulssart <73292964+pulssart@users.noreply.github.com>
2025-08-10 18:38:24 +0200

    feat(analytics): inject Google tag (gtag.js) G-B1SFE5MFGE en plus de GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           }}
         />
         {/* End Google Tag Manager */}
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-B1SFE5MFGE" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-B1SFE5MFGE');
+`}}
+        />
+        {/* End Google tag */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {/* Google Tag Manager (noscript) */}
